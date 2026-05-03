b67dc1315e30eb545cda0071acd5075cfb46c6ed
jess708godeto <jesuspoturo08@gmail.com>
2026-05-03 10:00:19 -0400

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/useAuthStore';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://neoua2.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
